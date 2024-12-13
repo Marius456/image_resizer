9bf8c9eb48fec5924ad98905509a008fed506e61
Marius456 <marius.zilguzis@gmail.com>
2024-12-13 09:52:21 +0200

    leave only resize cli

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 const resizeIcon = require('./src/utils/resizeIcon')
-const resize_drawable = require('./src/resize_drawable')
-const resize_mipmap = require('./src/resize_mipmap')
-const resize_appiconset = require('./src/resize_appiconset')
 
 
 module.exports = resizeIcon
-module.exports = resize_drawable
-module.exports = resize_mipmap
-module.exports = resize_appiconset
