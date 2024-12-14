901e1548fd74eb5ec49cbfae1616762acc99ae62
Marius456 <marius.zilguzis@gmail.com>
2024-12-14 14:24:59 +0200

    added ios

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const generateAndroid = require('./src/generateAndroid')
 const generateAndroidtv = require('./src/generateAndroidtv')
+const generateIos = require('./src/generateIos')
 
 
 const update_project = () => {
     // 500 × 487
     generateAndroid()
     generateAndroidtv()
+    generateIos()
     console.log('Updated project')
 }
 
---
+++ src/generateIos.js
@@ -0,0 +1,23 @@
+const resizeIcon = require('./utils/resizeIcon')
+const makeFolder = require('./utils/makeFolder')
+
+async function generateIos() {
+    const basePath = './files/output/builds/ios/RNVApp/Assets.xcassets/AppIcon.appiconset';
+    const iconPath = '/Users/mariuszilguzis/Desktop/zxc.png';
+
+    const sizes = [
+      16, 20, 29, 32, 40, 48, 50, 55, 57, 58, 60, 64, 66, 72, 76, 80, 
+      87, 88, 92, 100, 102, 114, 120, 128, 144, 152, 167, 172, 180, 
+      196, 216, 256, 512, 1024
+    ];
+  
+    makeFolder(basePath);
+    const resizePromises = sizes.map(size => 
+      resizeIcon(iconPath, basePath, size, size, size)
+    );
+  
+    await Promise.all(resizePromises);
+  }
+  
+
+module.exports = generateIos
